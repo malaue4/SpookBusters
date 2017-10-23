45b0fe4837846fdf078a627e2eb563f47b29e4df
alexander <alexander@ibsen.me>
2017-10-23 15:57:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alexanderibsen/spookbusters/GhostCamActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexanderibsen.spookbusters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GhostCamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ghost_cam);
+    }
+}
